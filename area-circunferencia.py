82b4460fdee82864b3703f6a9c7d8a7d7f848073
robertoBCV <90868053+robertoBCV@users.noreply.github.com>
2025-09-21 17:27:55 -0500

    programa para calcular area-circunferencia.py

---
+++ area-circunferencia.py
@@ -0,0 +1,12 @@
+print("Sistma para la Cincunferencia")
+print("=============================")
+print()
+print("""
+        █▀▀▀▄ █▀▀▀▀
+        █▄▄▄▀ █
+        █   █ █
+        ▀▀▀▀  ▀▀▀▀▀
+      """)
+radio = float(input("INgresa raidio: "))
+area = Math.PI* radio**2
+print("El area es: ",area.__round__(2))
