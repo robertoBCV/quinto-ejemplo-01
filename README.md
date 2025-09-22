2be4bacbeca522768e5fda6a21073d48175e5257
robertoBCV <robertobc8216@hotmail.com>
2025-09-21 19:05:00 -0500

    circunferncia con pie de pagina

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Readme para la ccircunferencia
+Este es mi primer Readme para este repositorio en github
--- area-circunferencia.py
+++ area-circunferencia.py
@@ -10,3 +10,6 @@ print("""
 radio = float(input("INgresa raidio: "))
 area = Math.PI* radio**2
 print("El area es: ",area.__round__(2))
+print()
+print("Visitanos en www.bctech.com")
+print("Telfemor empresarial 9977654")
